15b01e494aabe8693c9784a6bcd714a8af8616e4
Y.Mitsui <dyfomg@gmail.com>
2022-05-03 21:20:42 +0900

    デモをわかりやすく

--- src/example.py
+++ src/example.py
@@ -1,5 +1,31 @@
+### ２つのテキストの類似度を文字列カーネルを使って計算するデモ ###
 import string_kernel
-today = [bytes("今日は非常にいい天気だ","utf-8"), bytes("昨日はいい天気だ", "utf-8")]
-yesterday=[bytes("昨日はいい天気だ", "utf-8")]
-print(string_kernel.stringKernelUTF8Multi(today,yesterday, 2, 0.5))
-print(string_kernel.stringKernelUTF8(today[0],yesterday[0], 2, 0.5))
+
+### デモ１
+print("======= DEMO1 =======")
+text1 = "今日は非常にいい天気だ"
+text2 = "昨日はいい天気だ"
+print("テキスト１：%s"%(text1))
+print("テキスト２：%s"%(text2))
+# bytesに変換してから渡します
+similar = string_kernel.stringKernelUTF8(bytes(text1, "utf-8"), bytes(text2, "utf-8"), 2, 0.5)
+print("類似性スコア：%.4f"%(similar))
+
+### デモ2
+print("======= DEMO2 =======")
+text1 = "今日は非常にいい天気だ"
+text2 = "今日は非常にいい天気だ"
+print("テキスト１：%s"%(text1))
+print("テキスト２：%s"%(text2))
+similar = string_kernel.stringKernelUTF8(bytes(text1, "utf-8"), bytes(text2, "utf-8"), 2, 0.5)
+print("類似性スコア：%.4f"%(similar))
+
+### デモ3
+print("======= DEMO3 =======")
+text1 = "今日は非常にいい天気だ"
+text2 = "私は大きな喜びを感じている"
+print("テキスト１：%s"%(text1))
+print("テキスト２：%s"%(text2))
+similar = string_kernel.stringKernelUTF8(bytes(text1, "utf-8"), bytes(text2, "utf-8"), 2, 0.5)
+print("類似性スコア：%.4f"%(similar))
+
